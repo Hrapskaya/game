decbbb1c4aa7c81fe2d1fecd88766db8f46b01d3
alexxrap <xrapalexandra@gmail.com>
2020-12-18 09:37:24 +0300

    final

--- src/main/java/gmail/xrapalex/game/ArgumentsValidator.java
+++ src/main/java/gmail/xrapalex/game/ArgumentsValidator.java
@@ -12,29 +12,37 @@ public class ArgumentsValidator {
     private ArgumentsValidator() {
     }
 
-    public static ArgumentsValidator getInstance(){
+    public static ArgumentsValidator getInstance() {
         return instance;
     }
 
     public void isValid(String[] arguments) throws ValidateException {
-        isValidArgumentNumber(arguments);
-        isArgumentsRepeat(arguments);
+        isValidNumber(arguments);
+        isOddNumber(arguments);
+        isRepeat(arguments);
     }
 
-    private void isValidArgumentNumber(String[] arguments) throws ValidateException {
+    private void isOddNumber(String[] arguments) throws ValidateException {
+        if (arguments.length % 2 == 0){
+            throw new ValidateException("The number of arguments must be odd!"
+                    + '\n' + "For example: rock paper scissors.");
+        }
+    }
+
+    private void isValidNumber(String[] arguments) throws ValidateException {
         int length = arguments.length;
-        if(length < 3 || length % 2 == 0){
+        if (length < 3) {
             throw new ValidateException("Number of arguments must be 3 or more!"
                     + '\n' + "For example: rock paper scissors.");
         }
     }
 
-    private void isArgumentsRepeat(String[] arguments) throws ValidateException {
+    private void isRepeat(String[] arguments) throws ValidateException {
         Set<String> checkRepeat = new HashSet<>();
-        for (String s: arguments){
+        for (String s : arguments) {
             checkRepeat.add(s.toLowerCase());
         }
-        if(checkRepeat.size() < arguments.length){
+        if (checkRepeat.size() < arguments.length) {
             throw new ValidateException("Arguments must not be repeated!"
                     + '\n' + "For example: rock paper scissors.");
         }
